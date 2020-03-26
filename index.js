c66986d051cd572099a1249016d4c13ba787ac3e
Noah Scala <nwscala@gmail.com>
2020-03-26 13:45:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const targetP = document.querySelector('p')
+    targetP.textContent = "This is really cool!"
+});
